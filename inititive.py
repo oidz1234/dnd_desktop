5898ee22b62501b76c12236238780c04b0d508f0
Mark McNally <mark@mcnally.je>
2022-09-03 17:49:43 +0100

    fiex bug in the most stupid way posible

--- inititive.py
+++ inititive.py
@@ -8,7 +8,10 @@ while True:
         quit()
     elif text != 'D':
         text = text.split()
-        inititive[int(text[1])] = text[0]
+        if float(text[1]) in inititive:
+            text[1] = int(text[1]) + 0.1
+            print(text[1])
+        inititive[float(text[1])] = text[0]
     elif text == 'D':
         os.system('clear')
         for n in sorted(inititive, reverse=True):
